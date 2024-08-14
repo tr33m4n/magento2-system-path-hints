574e82b093ac185cee919b5ed821d311cf335fc9
Daniel Doyle <tr33m4n@googlemail.com>
2024-08-14 21:52:11 +0100

    Remove unused imports from Rector config

--- rector.php
+++ rector.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 use Rector\Caching\ValueObject\Storage\MemoryCacheStorage;
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
-use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
-use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
-use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -25,7 +20,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->paths([__DIR__ . '/src']);
     $rectorConfig->skip([
-        __DIR__ . '/src/Test',
-        FinalizeClassesWithoutChildrenRector::class
+        __DIR__ . '/src/Test'
     ]);
 };
